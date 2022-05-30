2a2c162342a7e6ac5eda80c10f8582f2d9a35ad1
Rodrigo Lopes <59925590+Kiro-tagama@users.noreply.github.com>
2022-05-30 10:46:27 -0300

    form

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  onAdicionarCliente(form:NgForm) {
+    if (form.invalid) {
+      return
+    }
+
+    this.clienteService.adicionarCliente(
+      form.value.nome,
+      form.value.email,
+      form.value.tel,
+      form.value.nacionalidade,
+      form.value.desc,
+    )
+    form.resetForm()
+
+    
+    // const cliente:Cliente={
+    //   nome:form.value.nome,
+    //   fone:form.value.fone,
+    //   email:form.value.email,
+    // }
+    // this.clienteAdicionado.emit(cliente)
+
+    //extra q eu coloquei para limpar o campo apos enviar
+    // this.nome=''
+    // this.fone=''
+    // this.email=''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
