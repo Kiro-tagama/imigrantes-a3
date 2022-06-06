2a046c1839d033e5e84366608bb8686f6664e530
jvlpinheiro <jvlpinheiro4@gmail.com>
2022-06-06 11:28:01 -0300

    up

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -48,7 +48,7 @@ export class FormComponent implements OnInit {
             bodyFormData.append(key, cliente[key]);
         return axios({
             method: "post",
-            url: 'https://console.firebase.google.com/project/imigrantes-9d6df/overview/add',
+            url: 'imigrantes-9d6df.appspot.com/add',
             data: bodyFormData,
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
         })
